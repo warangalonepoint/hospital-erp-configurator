d7357ddba1a30933e7c700031ee1a9e332408bd8
warangalonepoint <gullapallychaitanya@gmail.com>
2025-09-15 16:23:09 +0530

    Update barcode.js

--- charan_clinic_pwa/scripts/barcode.js
+++ charan_clinic_pwa/scripts/barcode.js
@@ -1,1 +1,8 @@
-<PASTE BARCODE.JS CONTENT FROM ABOVE>
+<script src="https://unpkg.com/jsqr@1.4.0/dist/jsQR.js"></script>
+<script>
+export async function decodeFrame(ctx, w, h){
+  const img = ctx.getImageData(0,0,w,h);
+  const code = jsQR(img.data, w, h);
+  return code?.data || null;
+}
+</script>
