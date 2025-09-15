7a6dd40fc0bc4ea1b1f122deeb0866ddbcf5c6e4
warangalonepoint <gullapallychaitanya@gmail.com>
2025-09-15 16:22:40 +0530

    Update utils.js

--- charan_clinic_pwa/scripts/utils.js
+++ charan_clinic_pwa/scripts/utils.js
@@ -1,1 +1,15 @@
-<PASTE UTILS.JS CONTENT FROM ABOVE>
+export const $ = (q,root=document)=>root.querySelector(q);
+export const $$ = (q,root=document)=>[...root.querySelectorAll(q)];
+export function toast(msg){ alert(msg); }
+export function clearAll(){
+  if(confirm("Clear cache and local data?")){
+    caches.keys().then(keys=>keys.forEach(k=>caches.delete(k)));
+    indexedDB.databases?.().then(dbs=>dbs.forEach(d=>indexedDB.deleteDatabase(d.name)));
+    location.reload();
+  }
+}
+export function walink(text, phone=""){
+  const t = encodeURIComponent(text);
+  const p = phone ? `&phone=${phone.replace(/\D/g,"")}` : "";
+  return `https://wa.me/?text=${t}${p}`;
+}
